a0ea9de83866bdcb8f01ea74a245dfa379206eb7
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2026-04-30 16:03:20 +0300

    Delete src/api/store/middleware.ts

--- src/api/store/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { defineMiddlewares } from "@medusajs/framework/http"
-import { repairMiddlewares } from "./admin/repairs/middlewares"
-import { storeRepairMiddlewares } from "./store/repairs/middlewares"
-
-export default defineMiddlewares({
-  routes: [...repairMiddlewares, ...storeRepairMiddlewares],
-})
