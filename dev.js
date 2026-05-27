00cd2b7f7d55c4e7bcbfbba3a137c4564e6bc01c
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2026-05-27 13:20:43 +0000

    update

--- dev.js
+++
@@ -1,3 +0,0 @@
-const { spawn } = require('child_process');
-const child = spawn('npx', ['medusa', 'plugin:develop'], { stdio: 'inherit', shell: true });
-child.on('exit', code => process.exit(code));
--- fetch-utils.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://raw.githubusercontent.com/enomshop-ltd/medusa-freshjs-storefront/refs/heads/main/utils.ts', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log(data));
-});
